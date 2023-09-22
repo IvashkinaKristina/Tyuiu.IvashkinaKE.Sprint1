0b79659796401919a95af0b4b4c95e2b9e0b3ac6
Tyuiu.IvashkinaKE@outlook.com <Tyuiu.IvashkinaKE@outlook.com>
2023-09-22 21:53:42 +0500

    Task3

---
+++ Tyuiu.IvashkinaKE.Sprint1.Task3.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvashkinaKE.Sprint1.Task3.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvashkinaKE.Sprint1.Task3.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42bc5ed9-2499-438d-b4a3-9f391fbf4206")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvashkinaKE.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.IvashkinaKE.Sprint1.Task3.V3.Lib;
+
+namespace Tyuiu.IvashkinaKE.Sprint1.Task3.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Ивашкина К.Э. | АСОиУб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Ивашкина Кристина Эдуардовна | АСОиУб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет вычисление объём параллелепипеда и печатает результат         *");
+            Console.WriteLine("* на экране.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double length = 9;
+            double width = 7.5;
+            double height = 5;
+            Console.WriteLine("Длина Х параллелепипеда = " + length);
+            Console.WriteLine("Ширина Y параллелепипеда = " + width);
+            Console.WriteLine("Высота Z параллелепипеда = " + height);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Объём параллелепипеда = " + ds.ParallelepipedVolume(length, width, height));
+            Console.ReadKey();
+        }
+    }
+}
